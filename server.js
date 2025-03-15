064f2cad023b6a9cbb3e91a29a96980e8fba93e9
sahil. <154417414+aisahilyadav@users.noreply.github.com>
2025-03-15 12:05:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,17 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: [ 'https://round-robin-coupon-distribution-git-main-sahilsky-ais-projects.vercel.app',
-           'https://round-robin-coupon-distribution-nu.vercel.app'],
+  origin: [ 'https://round-robin-coupon-distribution-red.vercel.app'],
   credentials: true
 }));
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Cache-Control', 'no-store'); // Disable caching for preflight responses
-  res.sendStatus(200);
-});
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
