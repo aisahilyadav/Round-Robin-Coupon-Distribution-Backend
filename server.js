09b6223f3149646e5080a563fbf51ad4e3282bce
sahil. <154417414+aisahilyadav@users.noreply.github.com>
2025-03-15 11:52:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: [ 'https://round-robin-coupon-distribution-git-main-sahilsky-ais-projects.vercel.app/',
+  origin: [ 'https://round-robin-coupon-distribution-git-main-sahilsky-ais-projects.vercel.app',
            'https://round-robin-coupon-distribution-nu.vercel.app'],
   credentials: true
 }));
